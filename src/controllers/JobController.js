85e6aa51fd214e3d345b7a10feb20d636b1ea1b9
fernandosantoshs <fernandosantoshs@gmail.com>
2021-12-14 11:47:11 -0300

    Refactoring : Job model and Job controller
    refactored  update and create jobs to async functions manipulating data from  a database

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -9,11 +9,9 @@ module.exports = {
 
   async save(req, res) {
     // Em JS, o símbolo "?" no fim de uma atribuicao, é um opcional 'Logical Chaining operator'(Encadeamento). Se o valor da primeira opção (antes do '?'), for falso, será atribuído o segundo valor que foi passado. Neste caso, o jobId será o length de jobs -1 (Assim tenho o índice no array que começa com 0), mas caso nao exista item no array, receberá '1' no push().
-    const jobs = await Job.get()
-    const lastId = jobs[jobs.length - 1]?.id || 0;
+    //const lastId = jobs[jobs.length - 1]?.id || 0;
 
-    Job.create({
-      id: lastId + 1,
+    await Job.create({
       name: req.body.name,
       "daily-hours": req.body["daily-hours"],
       "total-hours": req.body["total-hours"],
@@ -42,38 +40,22 @@ module.exports = {
 
   async update(req, res) {
     const jobId = req.params.id;
-    const jobs = await Job.get()
-
-    const job = jobs.find((job) => Number(job.id) === Number(jobId));
-
-    if (!job) {
-      return res.send("Job not found! :(");
-    }
-
+    
     const updatedJob = {
-      ...job,
       name: req.body.name,
       "total-hours": req.body["total-hours"],
       "daily-hours": req.body["daily-hours"],
     };
 
-    const newjobs = jobs.map((job) => {
-      if (Number(job.id) === Number(jobId)) {
-        job = updatedJob;
-      }
-
-      return job;
-    });
-
-    Job.update(newjobs)
+    await Job.update(updatedJob, jobId)
 
     res.redirect("/job/" + jobId);
   },
 
-  delete(req, res) {
+  async delete(req, res) {
     const jobId = req.params.id;
     
-    Job.delete(jobId)
+    await Job.delete(jobId)
     
     /* Refatorando : Como o responsavel pelos dados é o model, deve-se passar para ele as responsabilidade dos dados enviando o parametro para delete.* 
     const jobs = Job.get()
--- src/model/Job.js
+++ src/model/Job.js
@@ -1,33 +1,60 @@
-const Database = require('../db/config')
+const Database = require("../db/config");
 
 module.exports = {
   async get() {
-    const db = await Database()
+    const db = await Database();
 
     // Diferente do db.get() que só traz um dado, o db.all() traz todos os dados que satisfazem a query.
-    const jobs = await db.all(`SELECT * FROM jobs`)
-    
-    await db.close()
+    const jobs = await db.all(`SELECT * FROM jobs`);
+
+    await db.close();
     //Em uma arrow function sem estrutura de função (apenas retornando um objeto), não é necessário usar outro return nem {}. Ao invés disso, basta colocar parênteses envolvendo os colchetes do objeto retorno.
-    return jobs.map (job => ({      
-        id: job.id,
-        name: job.name,
-        "daily-hours": job.daily_hours,
-        "total-hours": job.total_hours,
-        create_at: job.created_at
-      })
-    )
+    return jobs.map((job) => ({
+      id: job.id,
+      name: job.name,
+      "daily-hours": job.daily_hours,
+      "total-hours": job.total_hours,
+      create_at: job.created_at,
+    }));
   },
 
-  update(newJob) {
-    data = newJob
-  }, 
+  async update(updatedJob, jobId) {
+    const db = await Database()
 
-  delete(id){
-    data = data.filter(job => Number(data.id) !== Number(id))
+    db.run(`UPDATE jobs SET 
+    name = "${updatedJob.name}",
+    daily_hours = ${updatedJob["daily-hours"]},
+    total_hours = ${updatedJob["total-hours"]}
+    WHERE id = ${jobId}
+    `)
+
+    await db.close()
   },
-  
-  create(newJob) {
-    data.push(newJob)
-  }
+
+  async delete(id) {
+    const db = await Database()
+
+    await db.run(`DELETE FROM jobs
+    where id = ${id}`)
+
+    await db.close()
+  },
+
+  async create(newJob) {
+    const db = await Database();
+
+    await db.run(`INSERT INTO jobs(
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+      ) VALUES (
+        "${newJob.name}",
+        ${newJob["daily-hours"]},
+        ${newJob["total-hours"]},
+        ${newJob.created_at}
+    )`);
+
+    await db.close();
+  },
 };
